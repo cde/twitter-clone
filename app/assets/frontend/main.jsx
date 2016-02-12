1eb364c837cd535411e779520e799ba867b3c5b3
cde <cdechauri@gmail.com>
2016-02-12 11:11:23 -0800

    Add mock tweets

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,17 @@
 import TweetBox from './components/TweetBox';
 import TweetList from './components/TweetList';
 
+let mockTweets = [
+  {id: 1, name: 'Carmen', body: "Hola Mundo"},
+  {id: 2, name: 'Ruben', body: "Hola Hermana"},
+  {id: 3, name: 'Enrique', body: "Hola Hermano"},
+]
 class Main extends React.Component {
   render(){
     return(
       <div className="container">
         <TweetBox />
-        <TweetList />
+        <TweetList tweets={mockTweets}/>
       </div>
     );
   }
