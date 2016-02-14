04a33655962bcbbc99feedff98d8b3ffa8eb21da
cde <cdechauri@gmail.com>
2016-02-13 16:32:01 -0800

    Add actions, API and store. Use stores to display view on Main.jsx

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,44 +1,56 @@
 import TweetBox from './components/TweetBox';
-import TweetList from './components/TweetList';
+import TweetsList from './components/TweetsList';
 
+import TweetStore from './stores/TweetStore';
+import TweetActions from './actions/TweetActions';
+TweetActions.getAllTweets();
+
+let getAppState = () => {
+  return { tweetsList: TweetStore.getAll() };
+}
 
 class Main extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      tweetList: []
-    };
+    this.state = getAppState();
+    this._onChange = this._onChange.bind(this);
   }
-  formattedTweets(tweetList){
-    let formattedlist = tweetList.map( tweet => {
-      tweet.formattedDate = moment(tweet.created_at).fromNow();
-      return tweet;
-    });
-    return {
-      tweetList: tweetList
-    };
-  }
+  // formattedTweets(tweetList){
+  //   let formattedlist = tweetList.map( tweet => {
+  //     tweet.formattedDate = moment(tweet.created_at).fromNow();
+  //     return tweet;
+  //   });
+  //   return {
+  //     tweetList: tweetList
+  //   };
+  // }
   
   addTweet(tweetToAdd) {
-    $.post("/tweets", { body: tweetToAdd })
-    .success( savedTweet => {
-      let actualTweetList = this.state.tweetList;
-      actualTweetList.unshift(savedTweet);
-      this.setState(this.formattedTweets(actualTweetList))
-    })
-    .error(error => console.log(error))
+    // $.post("/tweets", { body: tweetToAdd })
+    // .success( savedTweet => {
+    //   let actualTweetList = this.state.tweetList;
+    //   actualTweetList.unshift(savedTweet);
+    //   this.setState(this.formattedTweets(actualTweetList))
+    // })
+    // .error(error => console.log(error))
   }
   
-  componentDidMount(){
-    $.ajax("/tweets")
-    .success(data => this.setState(this.formattedTweets(data)))
-    .error(error => console.log(error));
+  componentDidMount() {
+    console.log(5, "componentDidMount Main._onChange");
+    TweetStore.addChangeListener(this._onChange);
+  }
+  componentWillUnmount() {
+    console.log(5, "componentWillUnmount Main._onChange");
+    TweetStore.removeChangeListener(this._onChange);  
+  }
+  _onChange() {
+    this.setState(getAppState());
   }
   render() {
     return(
       <div className="container">
         <TweetBox sendTweet={this.addTweet.bind(this)}/>
-        <TweetList tweets={this.state.tweetList}/>
+        <TweetsList tweets={this.state.tweetsList}/>
       </div>
     );
   }
