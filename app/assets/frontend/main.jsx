d95757e1d85867cae01592cacbb3991164b07bde
cde <cdechauri@gmail.com>
2016-02-16 11:50:34 -0800

    Add react-router

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// using an ES6 transpiler, like babel
+import { Router, Route, Link, browserHistory } from 'react-router'
+
 import Index from "./components/Index";
+import Follow from "./components/Follow";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> { this.props.children } </div>
+    )
+  }
+}
 
 let documentReady = () => {
   let reactNode = document.getElementById('react');
   if (reactNode) {
     ReactDOM.render (
-      <Index />, reactNode
+      <Router history={browserHistory} >
+        <Route component={App}>
+          <Route path="/" component={Index}></Route>
+          <Route path="/follow" component={Follow}></Route>
+        </Route>
+      </Router>,
+      reactNode
     );
   }
 };
